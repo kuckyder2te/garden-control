cad6509961770821846c2f8da7902a2e91d7e734
Willy <willy.kuckelsberg@gmail.com>
2025-12-08 18:23:25 +0100

    localUdpLogger enable Program run

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ void setup()
   delay(2000);
   Serial.begin(115200);
   DebugOutput->begin(DEBUG_SPEED);
-  Logger::setOutputFunction(&MyLoggerOutput::localLogger);
+  Logger::setOutputFunction(&MyLoggerOutput::localUdpLogger);
   Logger::setLogLevel(Logger::DEBUG); // Muss immer einen Wert in platformio.ini haben (SILENT)
   delay(500);                         // For switching on Serial Monitor
   LOGGER_NOTICE_FMT("************************* Garden Service (%s) *************************", __TIMESTAMP__);
